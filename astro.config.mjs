14e7e14327564cff1f69658660749edf710a9c48
Jillazquez <117284941+Jillazquez@users.noreply.github.com>
2024-10-23 18:55:58 +0200

    upload github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,6 @@
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://jillazquez.github.io',
+  base: '/portfolio/',
+});
