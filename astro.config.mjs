e9c51e392cafee9406fdb6b65f740126b8b29f9c
Jillazquez <jillazquez@gmail.com>
2024-11-01 23:30:43 +0100

    fix packages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
 import { defineConfig } from 'astro/config';
-import github from '@astrojs/github';
 
 export default defineConfig({
   site: 'https://jillazquez.github.io',
-  base: '/portfolio',
-  integrations: [github()],
+  base: '/'
 });
