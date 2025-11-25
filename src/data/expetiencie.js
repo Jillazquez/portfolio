f2ff6c37dbcc0b796bb3b03122e3a269b86347b4
juanma <jillazquez@gmail.com>
2025-11-25 16:45:12 +0000

    first commit

---
+++ src/data/expetiencie.js
@@ -0,0 +1,12 @@
+export const experience = [{
+    companyPeriod:"ISYC March 2025 - June 2025",
+    title:"Java Developer",
+    description:"Developed an ERP where I handled Spring, PostgreSQL databases, and web design."
+},
+{
+    companyPeriod:"KPMG September 2023 - June 2024",
+    title:"Dynamics Developer",
+    description:"Built multiple solutions using Microsoft Dynamics F&O and Electronic Reporting."
+    
+}
+]
